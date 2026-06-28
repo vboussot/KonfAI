41d9d83ac96b45a5ee1900b8d5f571d4e8c7dfcf
Valentin Boussot <valentin@fideus.io>
2026-06-28 12:51:15 +0200

    feat: add OME-Zarr (ngff-zarr) and DICOM dataset backends

--- konfai/utils/__init__.py
+++ konfai/utils/__init__.py
@@ -1,1 +1,5 @@
-"""Utility modules supporting configuration, datasets, ITK, and runtime helpers."""
+"""Utility modules supporting configuration, datasets, ITK, DICOM, OME-Zarr, and runtime helpers."""
+
+from konfai.utils import dicom, ome_zarr
+
+__all__ = ["dicom", "ome_zarr"]
